53d968e65bfba6e8510930f4f74bb6f5e97fa5fd
Guy <guythomas721@gmail.com>
2018-05-13 17:30:00 -0700

    Add ability to run basic test suite

--- test/index.js
+++ test/index.js
@@ -1,22 +1,5 @@
-var Pledge = require('../src/pledge');
+const adapter = require( './adapter' );
 
-module.exports = {
-  resolved: function(value) {
-    var pledge = new Pledge();
-    pledge.fulfill(value);
-    return pledge;
-  },
-  rejected: function(reason) {
-    var pledge = new Pledge();
-    pledge.reject(reason);
-    return pledge;
-  },
-  deferred: function() {
-    var pledge = new Pledge();
-    return {
-      promise: pledge,
-      resolve: pledge.fulfill,
-      reject: pledge.reject
-    };
-  }
-};
+describe("Promises/A+ Tests", function () {
+    require("./runner")(adapter);
+});
